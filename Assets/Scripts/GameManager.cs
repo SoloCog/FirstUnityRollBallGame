61732d09bc3d1b2c36ae71ff8149a2a187c4b942
SoloCog <zordoorstreams@gmail.com>
2021-12-27 16:23:42 -0600

    No gameplay impact. Behind the scenes now using a variable for gold required for each level. Will need to change this to an array but this is a start.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,12 @@ public class GameManager : MonoBehaviour
 {
     
     [SerializeField] private int playerGold;
+
+    //Would like to create an array that will hold how much gold is needed for each level
+    //Will want to do this so it can change this per level
+    //Will also need to see about updating DoorLockedScript to get goldRequiredForLevel auto instead of hard coded
+
+    [SerializeField] private int goldRequiredForLevel;
     [SerializeField] private TextMeshProUGUI playerGoldText;
     [SerializeField] private TextMeshProUGUI RoundTimerText;
 
@@ -25,7 +31,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerGoldText.text = "Gold " + playerGold.ToString() + "/6";
+        playerGoldText.text = "Gold " + playerGold.ToString() + "/" + goldRequiredForLevel;
         RoundTimerText.text = RoundTimer.ToString();
         
     }
@@ -35,7 +41,7 @@ public class GameManager : MonoBehaviour
     {
         RoundTimer = Time.time;
         
-        playerGoldText.text = "Gold " + playerGold.ToString() + "/6";
+        playerGoldText.text = "Gold " + playerGold.ToString() + "/" + goldRequiredForLevel;
         RoundTimerText.text = Mathf.FloorToInt(RoundTimer % 60 ).ToString() +"." + Mathf.FloorToInt((RoundTimer % 1) * 100).ToString(); 
     }
 }
